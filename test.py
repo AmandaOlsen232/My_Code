fixed jacobian function in .f90 file

test.py
@@ -9,5 +9,5 @@ def f(x):
     
 x = np.array([1, 2, 3]).reshape(3,1)
 
-# = my.jacobian(f, x)
-test = my.multivariable_newtons_method(f, x)
+J = my.jacobian(f, x)
+#test = my.multivariable_newtons_method(f, x)